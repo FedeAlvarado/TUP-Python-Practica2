3c48da5ab3593066bfee81181d7e523ee2652224
FedeAlvarado <federico.alvarado96@gmail.com>
2023-08-30 18:11:24 -0300

    Ejercicios

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,6 +2,10 @@
 
 
 def maximo_basico(a: float, b: float) -> float:
+    if a > b:
+        return a
+    return b
+    
     """Toma dos números y devuelve el mayor.
 
     Restricciones:
@@ -9,8 +13,7 @@ def maximo_basico(a: float, b: float) -> float:
         - No utilizar ELSE
         - No utilizar la función max
     """
-
-
+    
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
 assert maximo_basico(9, 18) == 18
@@ -24,7 +27,7 @@ def maximo_libreria(a: float, b: float) -> float:
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
-
+    return max(a,b)
 
 # NO MODIFICAR - INICIO
 assert maximo_libreria(10, 5) == 10
@@ -39,7 +42,7 @@ def maximo_ternario(a: float, b: float) -> float:
     """Re-escribir utilizando el operador ternario.
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
     """
-
+    return a if a > b else b
 
 # NO MODIFICAR - INICIO
 assert maximo_ternario(10, 5) == 10
